22bc04a26d1742dd111175de7d324d5a9c163cce
Joe Maller <joe@joemaller.com>
2015-02-11 00:19:08 -0500

    fixed up some bugs, started switching math over to math.js

--- source/js/metrocard-zero.js
+++ source/js/metrocard-zero.js
@@ -12,39 +12,49 @@ React.initializeTouchEvents(true);
 var MetrocardApp = React.createClass({
 
   getInitialState: function() {
-    return {transactions: [], balance: ''};
+    return {transactions: [], balance: 0};
   },
 
   componentDidMount: function() {
     events.addListener('digit', this.pushDigit);
-    events.addListener('popDigit', this.popDigit);
+    events.addListener('backspace', this.popDigit);
   },
 
   componentWillUnmount: function() {
     events.removeListener('digit', this.pushDigit);
-    events.removeListener('popDigit', this.popDigit);
+    events.removeListener('backspace', this.popDigit);
   },
 
   pushDigit: function(digit) {
-    console.log(digit, this.state.balance);
-    this.setState({balance: this.state.balance + '' + digit});
-    console.log('post balance', this.state.balance);
-    this.updateBuys(calculate_spending(this.state.balance));
+    var b = math.chain(this.state.balance)
+                .multiply(1000)
+                .add(digit)
+                .divide(100)
+                .done();
+    this.updateBuys(b);
   },
 
   popDigit: function() {
-    console.log('popDigit');
-    this.setState({balance: ('' + this.state.balance).slice(0,-1)});
+    var b = math.chain(this.state.balance)
+                .multiply(10)
+                .floor()
+                .divide(100)
+                .done();
+    this.updateBuys(b);
   },
 
-  updateBuys: function(data) {
-    this.setState({ transactions: data});
+  updateBuys: function(balance) {
+    this.setState({
+        balance: balance,
+        transactions: calculateSpending(balance)
+    });
   },
 
   render: function() {
     return(
       <div className="app">
-        <ValueForm updateBuys={this.updateBuys} balance={this.state.balance} />
+      debug balance: {this.state.balance}
+        <BalanceDisplay balance={this.state.balance} />
         <KeyPad />
         <BuyList transactions={this.state.transactions} />
         <hr />
@@ -55,39 +65,17 @@ var MetrocardApp = React.createClass({
 });
 
 
-var ValueForm = React.createClass({
-
-    handleSubmit: function(e) {
-        e.preventDefault();
-        var value = this.refs.value.getDOMNode().value;
-        value = parseFloat(value/100, 10);
-        // console.log(value, this.refs.value.getDOMNode().value);
-        this.props.updateBuys(calculate_spending(value));
-    },
-
-  getInitialState: function() {
-    return {balance: null};
-  },
-
-    componentDidMount: function() {
-      $('#initial-value').focus();
-    },
-
+var BalanceDisplay = React.createClass({
     render: function() {
-        // var money = '$' + this.props.balance / 100;
-        var money = numeral(this.props.balance / 100).format('$0.00');
+        var money = numeral(this.props.balance).format('$0.00');
         return (
-            <div>
-            <div>
-              {money}
-              <BackspaceButton />
-            </div>
-            </div>
+          <div>
+              <div className="balance">
+                  {money}
+                  <BackspaceButton />
+              </div>
+          </div>
         );
-            // <form className="metrocard-value" onChange={this.handleSubmit}>
-            //   <label for="initial-value">What’s on your Metrocard?</label>
-            //   <input id="initial-value" className="form-control" type="number" ref="value" placeholder="$ 0.00" value={this.props.balance} />
-            // </form>
     }
 });
 
@@ -96,7 +84,6 @@ var BuyList = React.createClass({
 
   render: function() {
     var showDebug = this.props.debug;
-    console.log('this.props.tranactions', this.props.transactions);
     var transactions = this.props.transactions.map(function(t) {
       return (
         <BuyCard data={t} debug={showDebug} />
@@ -118,14 +105,13 @@ var BuyCard = React.createClass({
   render: function() {
     var items = [];
     if (this.props.debug === true) {
-      items = _.omit(this.props.data, ['initial_value', 'rides'])
-      items = _.map(items, function(val, key) {
+      // items = _.omit(this.props.data, ['initial_value', 'rides'])
+      items = _.map(this.props.data, function(val, key) {
         return (
           <li><small><strong>{key}:</strong> {val}</small></li>
         );
       });
     }
-    console.log('items', items);
     return (
       <div className="transaction">
       <strong>Buy this: ${this.props.data.purchase}</strong> (${this.props.data.total_with_bonus.toFixed(2)} total, {this.props.data.rides} rides)
@@ -140,21 +126,24 @@ var BuyCard = React.createClass({
 var KeyPad = React.createClass({
   render: function() {
     return (
-      <div id="keypad">
-         <NumberButton value="1" />
-         <NumberButton value="2" />
-         <NumberButton value="3" />
-         <br />
-         <NumberButton value="4" />
-         <NumberButton value="5" />
-         <NumberButton value="6" />
-         <br />
-         <NumberButton value="7" />
-         <NumberButton value="8" />
-         <NumberButton value="9" />
-         <br />
-         <NumberButton value="0" />
-      </div>
+        <div id="keypad">
+            <div>
+                <NumberButton value={1} />
+                <NumberButton value={2} />
+                <NumberButton value={3} />
+            </div>
+            <div>
+                <NumberButton value={4} />
+                <NumberButton value={5} />
+                <NumberButton value={6} />
+            </div>
+            <div>
+                <NumberButton value={7} />
+                <NumberButton value={8} />
+                <NumberButton value={9} />
+            </div>
+            <NumberButton value="0" />
+        </div>
     );
   }
 });
@@ -167,13 +156,7 @@ var NumberButton = React.createClass({
       },
 
       clickHandler: function() {
-        // console.log(this.props.value);
-                events.emit('digit', this.props.value);
-
-        //         this.setState({
-        //   'active': true
-        // });
-
+        events.emit('digit', this.props.value);
       },
 
       touchStart: function() {
@@ -192,26 +175,26 @@ var NumberButton = React.createClass({
           var activeClass = (this.state.active) ? ' active' : '';
           return (
               <div
-              className={"number-button" + activeClass}
-                onTouchStart={this.touchStart}
-                onTouchEnd={this.touchEnd}
-                onClick={this.clickHandler}><span>{this.props.value}</span></div>
+                  className={"number-button" + activeClass}
+                  onTouchStart={this.touchStart}
+                  onTouchEnd={this.touchEnd}
+                  onClick={this.clickHandler}>
+                    <span>{this.props.value}</span>
+              </div>
     );
   }
 })
 
 var BackspaceButton = React.createClass({
   clickHandler: function() {
-    events.emit('popDigit');
+    events.emit('backspace');
   },
   render: function() {
     return (
-            <a className="back-button" onClick={this.clickHandler}>⌫</a>
-            // <button className="back-button" onClick={this.clickHandler}>⌫</button>
+      <a className="back-button" onClick={this.clickHandler}>⌫</a>
     );
   }
 });
-
 
 React.render(
   <MetrocardApp />,
@@ -228,11 +211,12 @@ var bonus = 1.05;  // Actual value of dollars spent above bonus_min
 var max_card_value = 50; // Maximum Metrocard value that will be calculated
 
 /**
- * [calculate_spending description]
+ * [calculateSpending description]
  * @param  {[type]} initial_value [description]
  * @return {[type]}               [description]
  */
-var calculate_spending = function(initial_value) {
+var calculateSpending = function(initial_value) {
+  console.log('!!!!! initial_value:', initial_value);
   var transactions = []; // suggested purchase amounts
   console.log(initial_value);
   for (var i = 2; i <= (max_card_value / fare); i++) {
@@ -252,7 +236,7 @@ var calculate_spending = function(initial_value) {
         raw_bonus: purchase * 0.05,
         total_sans_bonus: initial_value + purchase,
         total_with_bonus: target,
-        rides: target/fare,
+        rides: target / fare,
         initial_value: initial_value
       }
       transactions.push(transaction);
